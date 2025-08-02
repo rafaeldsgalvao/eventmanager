42961ff654d45cc3efb363787497dd2e126b7f92
Rafael Galvão <rafaeldsgalvao@gmail.com>
2025-08-02 17:27:18 -0300

    [FEAT]::Adicionando Teste para a aplicação

--- src/main/java/com/rafaelgalvao/eventmanager/controller/EventController.java
+++ src/main/java/com/rafaelgalvao/eventmanager/controller/EventController.java
@@ -8,6 +8,7 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -28,6 +29,7 @@ public class EventController {
     }
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public EventResponseDTO create(@Valid @RequestBody CreateEventDTO dto) {
         return eventService.create(dto);
     }
